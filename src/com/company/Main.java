9a4be10bd8563ec7066c37d3e02520508e182b0d
Kamil Maksymowicz <maxio090124@interia.pl>
2020-03-21 18:33:25 +0100

    klasy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,64 +15,46 @@ import java.net.URL;
 public class Main {
 
     private static HttpURLConnection connection;
+
     public static void main(String[] args) {
         //METHOD 1: java.net.HttpURLConnection
-    BufferedReader reader;
-    String line;
-    StringBuffer responseContent = new StringBuffer();
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
         try {
-        URL url= new URL("https://jsonplaceholder.typicode.com/users");
-        connection = (HttpURLConnection) url.openConnection();
+            URL url = new URL("https://jsonplaceholder.typicode.com/users");
+            connection = (HttpURLConnection) url.openConnection();
 
-        //Request setup
-        connection.setRequestMethod("GET");
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
+            //Request setup
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
 
-        int status=connection.getResponseCode();
-        // System.out.println(status );
-        if (status > 299){
-            reader= new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-            while((line = reader.readLine()) != null)  {
-                responseContent.append(line);
+            int status = connection.getResponseCode();
+            // System.out.println(status );
+            if (status > 299) {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+            } else {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                    responseContent.append(line + "\n");
+                }
+                reader.close();
             }
-        }else {
-            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while((line = reader.readLine()) != null)  {
-                responseContent.append(line+"\n");
-            }
-            reader.close();
+            Users parse = new Users(responseContent.toString());
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
         }
-        System.out.println(responseContent.toString());
-
-    } catch (MalformedURLException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
-        connection.disconnect();
     }
-}
-    String a="andrzej";
-
-    Users maciek = new Users();
-    Users.Address adres = maciek.new Address();
-
-    public static String parse(String responseBody)     {
-
-        JSONArray users = new JSONArray(responseBody);
-        for(int i = 0;i<users.length();i++){
-            JSONObject uzytkownik = users.getJSONObject(i);
-            int id = uzytkownik.getInt("id");
-            String name = uzytkownik.getString("name");
-            String username = uzytkownik.getString("username");
-            String email = uzytkownik.getString("email");
-
-        }
-        return null;
-    }
-
-
-
 
 }
--- src/com/company/Users.java
+++ src/com/company/Users.java
@@ -1,15 +1,45 @@
 package com.company;
 
-public class Users {
-    Integer id;
-    String name;
-    String username;
-    String email;
-    class Address{
-        String street;
-        String suite;
-        String city;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public  class Users extends Main{
+
+    Users(String responseBody){
+        JSONArray users = new JSONArray(responseBody);
+        for(int i = 0;i<users.length();i++){
+            JSONObject uzytkownik = users.getJSONObject(i);
+            int id = uzytkownik.getInt("id");
+            String username = uzytkownik.getString("username");
+            String name = uzytkownik.getString("name");
+
+            String email = uzytkownik.getString("email");
+            String phone = uzytkownik.getString("phone");
+            String website = uzytkownik.getString("website");
+            JSONObject adress = uzytkownik.getJSONObject("address");
+            String street = adress.getString("street");
+            String suite = adress.getString("suite");
+            String city = adress.getString("city");
+            String zipcode = adress.getString("zipcode");
+            JSONObject geo = adress.getJSONObject("geo");
+            String lat = geo.getString("lng");
+            String lng = geo.getString("lng");
+            JSONObject company = uzytkownik.getJSONObject("company");
+            String c_name = company.getString("name");
+            String catchPhrase = company.getString("catchPhrase");
+            String bs = company.getString("bs");
+
+
+            System.out.println(id+" "+name+" "+username+" "+email+" "+phone+" "+website+" "+street+" "+suite+" "+city+" "+zipcode );
+
+
+
+
+        }
+
 
     }
+
+
 
 }
